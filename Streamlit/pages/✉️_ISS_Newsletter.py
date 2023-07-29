e1e70671126476d73280347b22a7810b4287933e
GermanPaul12 <german.paul@outlook.de>
2023-07-30 00:12:37 +0200

    last version of html mail

--- Streamlit/pages/✉️_ISS_Newsletter.py
+++ Streamlit/pages/✉️_ISS_Newsletter.py
@@ -34,17 +34,17 @@ def save_email_to_database(email):
     <html>
     <head>
     </head>
-    <body style="font-family: 'Arial', sans-serif; color: #333333; margin: 0; padding: 0; background-image: url('https://raw.githubusercontent.com/GermanPaul12/ISS-Route-Viewer-Streamlit/main/Data/iss.jpg'); background-size: cover; background-repeat: no-repeat;">
-        <div style="background-color: rgba(0, 123, 255, 0.85); color: white; padding: 20px; text-align: center; font-size: 24px;">Hey there,</div>
+    <body style="font-family: 'Arial', sans-serif; color: #333333; margin: 0; padding: 0; background-color: rgba(192, 192, 192, 0.9); background-image: url('https://raw.githubusercontent.com/GermanPaul12/ISS-Route-Viewer-Streamlit/main/Data/iss.jpg'); background-size: cover; background-repeat: no-repeat;">
+        <div style="background-color: rgba(0, 0, 0, 0.8); color: white; padding: 20px; text-align: center; font-size: 24px;">Hey there,</div>
         <div style="background-color: rgba(255, 255, 255, 0.8); padding: 20px;">
             <p style="font-size: 16px;">Thanks for signing up to the newsletter.</p>
             <p style="font-size: 16px;">You will be notified when the ISS is above Mannheim.</p>
-            <p style="font-size: 16px;">Best regards,</p>
-            <p style="font-size: 16px; font-weight: bold;">German</p>
+            <p style="font-size: 16px; font-weight: bold;">Best regards</p>
+            <p style="font-size: 16px;">German</p>
             <p style="font-size: 12px; color: #888888;">(ISS Mannheim Newsletter)</p>
         </div>
         <div style="background-color: rgba(255, 255, 255, 0.8); color: #333333; padding: 10px; text-align: center; font-size: 12px;">
-            <p style="margin: 0;">Remove yourself from the Newsletter: <a href="https://iss-route.streamlit.app/ISS_Newsletter" style="color: #007BFF; text-decoration: none;">Unsubscribe</a></p>
+            <p style="margin: 0; color: #007BFF;">Remove yourself from the Newsletter: <a href="https://iss-route.streamlit.app/ISS_Newsletter" style="color: #007BFF; text-decoration: none;">Unsubscribe</a></p>
         </div>
     </body>
     </html>
